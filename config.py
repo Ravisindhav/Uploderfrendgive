ed1168e3572f5217f8a906db88c030ce140f2a25
Ravisindhav <sindhavravi11@gmail.com>
2025-07-12 04:38:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,22 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "11745176"))
+API_ID = int(os.environ.get("API_ID", "27660379"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "04bfe15f858b8389d393a217f929daea")
+API_HASH = os.environ.get("API_HASH", "19c71c27733f0954371085198855125a")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8184337390:AAHN-xN8mkuxTymdqFNKN9V-3mVtB22oN04")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8138308196:AAFssD4sb6v84eiVCzXl3WFLl05rSTUPlDc")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("@madarchotbsdk_bot")
+BOT_USERNAME = os.environ.get("uploderfrendgive_bot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "1757713411"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5459854363"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1757713411").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5459854363").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002526688628"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002761572365"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://akash1:cluster8343@cluster0.eupasvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002898145222"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002761572365"))
 
