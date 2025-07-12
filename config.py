3dc8d52990bc363daaf9754a48bb4a388f9151e1
Ravisindhav <sindhavravi11@gmail.com>
2025-07-13 00:15:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = os.environ.get("API_HASH", "19c71c27733f0954371085198855125a")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8138308196:AAFssD4sb6v84eiVCzXl3WFLl05rSTUPlDc")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("uploderfrendgive_bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "5459854363"))
 # ------------------X------------------------------
